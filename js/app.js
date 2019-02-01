e59cd0139f7a8bb2275c8246b6be59b1de4a4278
KingBinwei <k19990101@icloud.com>
2019-02-01 14:17:08 +0800

    当玩家与敌人碰撞时，玩家位置初始化

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ var Enemy = function() {
 // 此为游戏必须的函数，用来更新敌人的位置
 // 参数: dt ，表示时间间隙
 Enemy.prototype.update = function(dt) {
+    //你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+    // 都是以同样的速度运行的
     this.initalPositionX += this.moveSpeed * dt; 
     if (this.initalPositionX >= 505 || (this.initalPositionX >= rock.initalPositionX - 101 && this.initalPositionY + 20 == rock.initalPositionY + 45)) {
         this.initalPositionX = -101;
@@ -22,8 +24,11 @@ Enemy.prototype.update = function(dt) {
         this.moveSpeed = Math.random()*200+200;
         this.initalPositionX += this.moveSpeed * dt;
     }
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
-    // 都是以同样的速度运行的
+    if (rectangleCol(this.initalPositionX, this.initalPositionY + 20, 101, 83, player.initalPositionX, player.initalPositionY + 41.5, 101, 83)) {
+        player.initalPositionX = 202;
+        player.initalPositionY = 373.5;
+    }
+    /
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
